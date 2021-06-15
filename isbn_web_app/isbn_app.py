c36b8298ba70dc3ea4e7cf8979c6b975c3a1488b
telemichi <michaeln@live.de>
2021-06-15 11:54:19 +0200

    isbn

--- isbn_web_app/isbn_app.py
+++ isbn_web_app/isbn_app.py
@@ -5,11 +5,23 @@ Created on Tue Jun 15 09:50:28 2021
 @author: micha
 """
 
-from flask import Flask
+from flask import Flask, render_template
+
+
 
 print(__name__)
 app = Flask(__name__)
 
-@app.route("/") # Decorator
+@app.route("/") # Start Page
 def start_page():
-    return"<p>Hello World! Hope you are good!!!</p>"
+    return"<p>Hello World! Hope you are good, Sir!!!</p>"
+ 
+    
+ 
+@app.route("/info") # Information Page
+def show_info():
+    return "<p>Some information</p>"
+ 
+@app.route("/isbn/<isbn>")    
+def isbn_display(isbn):
+    return render_template("isbn_display.html")
